dc9e0751a6da17d2a90a2fca10009e004a9d1c1a
valeelim <valeriansalim@gmail.com>
2022-02-22 15:42:39 +0700

    add job and user route functions

---
+++ routes/user.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const passport = require('passport');
+const users = require('../controllers/users');
+const router = express.Router();
+
+
+router.post('/login', users.login);     // logs user in
+
+router.post('/register', users.register);       // registers user
+
+router.route('/:id')
+    .get((req, res) => {  // TODO get a profile detail with id from database
+        res.send('test')
+    })
+    .put((req, res) => {  // TODO update a profile detail with id from database
+        res.send('test')
+    })
+    .delete((req, res) => { // TODO delete a profile with id from database
+        res.send('test')
+    })
+
+module.exports = router;

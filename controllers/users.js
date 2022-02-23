25ad6b5b9b6887855acae22f82ed4973f097d899
valeelim <valeriansalim@gmail.com>
2022-02-23 13:09:21 +0700

    fixed username instead of email

--- controllers/users.js
+++ controllers/users.js
@@ -2,25 +2,28 @@ const passport = require('passport');
 const ExpressError = require('../utils/ExpressError');
 const User = require('../models/user');
 
+const changeUsername = err => err.message.replace('username', 'email');
+
 module.exports.register = async (req, res, next) => {
     try {
-        const { password, email } = req.body;
-        const user = new User({ email });
+        const { password } = req.body;
+        const user = new User({ ...req.body });
         await User.register(user, password);
-        console.log(user);
-        res.status(200).json(user);
+        const { hash, salt, ...userData } = user._doc;
+        res.status(200).json(userData);
     } catch (e) {
-        return next(e);
+        return next(new ExpressError(changeUsername(e), 400));
     }
 }
 
 module.exports.login = (req, res, next) => {
     passport.authenticate('local', function(err, user, info){
         if (err) return next(err);
-        if (!user) return next(new ExpressError(info.message, 401));
+        if (!user) return next(new ExpressError(changeUsername(info), 401));
         req.login(user, err => {
             if (err) return next(err);
-            return res.status(200).json(user);
+            const { hash, salt, ...userData } = user._doc;
+            return res.status(200).json(userData);
         })
     })(req, res, next);
 }

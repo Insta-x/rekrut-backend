eaf5a62b198e5dac36ca595d36d8f41570f23356
valeelim <valeriansalim@gmail.com>
2022-02-23 13:07:26 +0700

    adds job schema

---
+++ models/job.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { schema } = require('./user');
+const Schema = mongoose.Schema;
+
+const jobSchema = new Schema({
+    title: String,
+    detail: String,
+    location: String,
+    jobType: String,
+    category: String,
+    salary: String,
+    responsibility: String,
+    qualification: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    registrants: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Worker'
+        }
+    ],
+    chosen: {
+        type: Schema.Types.ObjectId,
+        ref: 'Worker'
+    },
+    review: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+})

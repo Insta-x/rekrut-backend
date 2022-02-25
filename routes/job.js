59ffb41c4cc7c7a421caaba7bd9b4953e4728c04
Insta-x <uttsadajason@outlook.com>
2022-02-25 21:19:53 +0700

    Init job routes

--- routes/job.js
+++ routes/job.js
@@ -1,26 +1,63 @@
 const express = require('express')
+const ExpressError = require('../utils/ExpressError');
+const Job = require('../models/job')
 const router = express.Router()
 
 
-router.get('/', (req, res) => {  // TODO get jobs
-    res.send('test')
+router.get('/', async (req, res, next) => {  // TODO get jobs
+    try {
+        const jobs = await Job.find()
+        res.status(200).send(jobs)
+    } catch (e) {
+        console.log(e)
+        return next(new ExpressError(e, 400));
+    }
 })
 
-router.post('/', (req, res) => {  // TODO vreate a new job
-    res.send('test')
+router.post('/', async (req, res, next) => {  // TODO create a new job
+    let job = new Job({
+        title : req.body.title,
+        detail : req.body.detail,
+        location : req.body.location,
+        jobType : req.body.jobType,
+        category : req.body.category,
+        salary : req.body.salary,
+        responsibility : req.body.responsibility,
+        qualification : req.body.qualification,
+        author : req.body.author,
+    })
+    
+    try {
+        job = await job.save();
+        res.status(200).send(job);
+    } catch (e) {
+        return next(new ExpressError(e, 400));
+    }
 })
 
 router
     .route('/:id')
-    .get((req, res) => {  // TODO get a job detail with id from database
-        res.send('test')
+    .get(async (req, res, next) => {  // TODO get a job detail with id from database
+        try {
+            const job = await Job.findById(req.params.id)
+            res.status(200).send(job)
+        } catch (e) {
+            return next(new ExpressError(e, 400));
+        }
     })
-    .put((req, res) => {  // TODO update a job detail with id from database
-        res.send('test')
+    .put(async (req, res) => {  // TODO update a job detail with id from database
+        res.send('job update')
     })
-    .delete((req, res) => { // TODO delete a job with id from database
-        res.send('test')
+    .delete(async (req, res, next) => { // TODO delete a job with id from database
+        try {
+            await Job.findByIdAndDelete(req.params.id)
+            res.status(200).json({
+                "message" : "Successful"
+            })
+        } catch (e) {
+            console.log(e)
+            return next(new ExpressError(e, 400));
+        }
     })
-
 
 module.exports = router

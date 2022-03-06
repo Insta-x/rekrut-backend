61d53050c85115bba369b92d89f56437e10538b4
valeelim <valeriansalim@gmail.com>
2022-03-06 16:40:19 +0700

    added authentication middlewares

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const ExpressError = require('./utils/ExpressError');
+const User = require('./models/user');
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated())
@@ -10,5 +11,13 @@ module.exports.isUser = async (req, res, next) => {
     const { id } = req.params;
     if (!req.user._id.equals(id))
         next(new ExpressError('You are not authorized', 401));
+    next();
+}
+
+module.exports.isClient = async (req, res, next) => {
+    const { id } = req.params;
+    const user = User.findById(id);
+    if (!user.client) 
+        next(new ExpressError('You must be a client', 401));
     next();
 }

044a654370f1c4aa20c2b598a0164a23ec4903fe
valeelim <valeriansalim@gmail.com>
2022-03-07 15:38:27 +0700

    added isClient

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 const ExpressError = require('./utils/ExpressError');
 const User = require('./models/user');
+const Job = require('./models/job');
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated())
@@ -15,9 +16,16 @@ module.exports.isUser = async (req, res, next) => {
 }
 
 module.exports.isClient = async (req, res, next) => {
+    const user = await User.findById(req.user._id);
+    if (user.worker) 
+        return next(new ExpressError('You must be a client', 401));
+    next();
+}
+
+module.exports.isJobOwner = async (req, res, next) => {
     const { id } = req.params;
-    const user = User.findById(id);
-    if (!user.client) 
-        next(new ExpressError('You must be a client', 401));
+    const job = await Job.findById(id);
+    if (!req.user._id.equals(job.author))
+        return next(new ExpressError('You are not authorized', 401));
     next();
 }
